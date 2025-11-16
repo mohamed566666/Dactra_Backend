d22231e06b7da65cb466cbc654f4aaae47bd63fd
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2025-11-16 17:17:19 +0200

    add Medical Tests Provider Contraller

---
+++ Dactra/Controllers/MedicalTestsProviderController.cs
@@ -0,0 +1,38 @@
+﻿using Dactra.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dactra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalTestsProviderController : ControllerBase
+    {
+        private readonly IMedicalTestsProviderService _medicalTestsProviderService;
+        public MedicalTestsProviderController(IMedicalTestsProviderService medicalTestsProviderService)
+        {
+            _medicalTestsProviderService = medicalTestsProviderService;
+        }
+
+        [HttpPost("CompleteRegister")]
+        public async Task<IActionResult> CompleteRegister(DTOs.ProfilesDTO.MedicalTestProviderDTO medicalTestProviderDTO)
+        {
+            await _medicalTestsProviderService.CompleteRegistrationAsync(medicalTestProviderDTO);
+            return Ok();
+        }
+
+        [HttpGet("GetAllProfiles")]
+        public async Task<IActionResult> GetAllProfiles()
+        {
+            var MedicalTestProviderProfiles = await _medicalTestsProviderService.GetAllProfilesAsync();
+            return Ok(MedicalTestProviderProfiles);
+        }
+
+        [HttpDelete("DeleteMedicalTestProvider/{medicalTestProviderId}")]
+        public async Task<IActionResult> DeleteMedicalTestProvider(int medicalTestProviderId)
+        {
+            await _medicalTestsProviderService.DeleteMedicalTestProviderProfileAsync(medicalTestProviderId);
+            return Ok("Profile Deleted Succesfully");
+        }
+    }
+}
--- Dactra/Services/Implementation/MedicalTestsProviderService.cs
+++ Dactra/Services/Implementation/MedicalTestsProviderService.cs
@@ -1,4 +1,7 @@
-﻿using Dactra.Repositories.Interfaces;
+﻿using Dactra.DTOs.ProfilesDTO;
+using Dactra.Enums;
+using Dactra.Models;
+using Dactra.Repositories.Interfaces;
 using Dactra.Services.Interfaces;
 using Microsoft.IdentityModel.Tokens;
 
@@ -7,9 +10,57 @@ namespace Dactra.Services.Implementation
     public class MedicalTestsProviderService : IMedicalTestsProviderService
     {
         private readonly IMedicalTestProviderProfileRepository _medicalTestProviderProfileRepository;
-        public MedicalTestsProviderService(IMedicalTestProviderProfileRepository medicalTestProviderProfileRepository )
+        private readonly IUserRepository _userRepository;
+        public MedicalTestsProviderService(IMedicalTestProviderProfileRepository medicalTestProviderProfileRepository,IUserRepository userRepository)
         {
             _medicalTestProviderProfileRepository = medicalTestProviderProfileRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task CompleteRegistrationAsync(MedicalTestProviderDTO medicalTestProviderDTO)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(medicalTestProviderDTO.Email);
+            if (user == null)
+            {
+                throw new ArgumentException("User Not Found");
+            }
+            if (!user.IsVerified)
+            {
+                throw new InvalidOperationException("Please verify your account first");
+            }
+            var existingProfile = await _medicalTestProviderProfileRepository.GetByUserIdAsync(user.Id);
+            if (existingProfile != null)
+            {
+                throw new InvalidOperationException("This User Already has an Profile");
+            }
+            var medicalTestProviderProfile = new MedicalTestProviderProfile
+            {
+                UserId = user.Id,
+                User = user,
+                Name = medicalTestProviderDTO.Name,
+                LicenceNo = medicalTestProviderDTO.licenceNo,
+                Address = medicalTestProviderDTO.Address,
+                About = medicalTestProviderDTO.About,
+                Type = medicalTestProviderDTO.rule.ToLower() == "lab" ? MedicalTestProviderType.Lab : MedicalTestProviderType.Scan
+            };
+            await _medicalTestProviderProfileRepository.AddAsync(medicalTestProviderProfile);
+            user.IsRegistrationComplete = true;
+            await _userRepository.UpdateUserAsync(user);
+        }
+
+        public async Task DeleteMedicalTestProviderProfileAsync(int medicalTestProviderProfileId)
+        {
+            var profile = await _medicalTestProviderProfileRepository.GetByIdAsync(medicalTestProviderProfileId);
+            if (profile == null)
+            {
+                throw new ArgumentException("Medical Test Provider Profile Not Found");
+            }
+            await _medicalTestProviderProfileRepository.DeleteAsync(profile);
+        }
+
+        public async Task<IEnumerable<MedicalTestProviderProfile>> GetAllProfilesAsync()
+        {
+            return await _medicalTestProviderProfileRepository.GetAllAsync();
         }
     }
 }
