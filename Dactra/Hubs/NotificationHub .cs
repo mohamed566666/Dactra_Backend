cd13b4422a9e86e5947c220145f08b458ecd9481
rashadmo8 <rashadmostafa84@gmail.com>
2026-04-26 21:41:15 +0300

    editSendtoDoctorNoti

--- Dactra/Hubs/NotificationHub .cs
+++ Dactra/Hubs/NotificationHub .cs
@@ -4,6 +4,7 @@
     {
         public async Task SendNotification(string userId, string message)
         {
+            Console.WriteLine($"Connected User: {Context.UserIdentifier}");
             await Clients.User(userId).SendAsync("ReceiveNotification", message);
         }
     }
