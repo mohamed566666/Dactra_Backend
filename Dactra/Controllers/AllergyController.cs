fc770f8898f99f86a4e2a23107776e6638482fbb
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2026-05-03 19:07:53 +0300

    fix problem of chaching

--- Dactra/Controllers/AllergyController.cs
+++ Dactra/Controllers/AllergyController.cs
@@ -1,50 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-
-namespace Dactra.Controllers
+﻿namespace Dactra.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AllergyController : ControllerBase
     {
         private readonly IAllergyService _service;
-        private readonly IMemoryCache _cache;
 
-        private const string AllergiesCacheKey = "AllergiesList";
-
-        public AllergyController(IAllergyService service, IMemoryCache cache)
+        public AllergyController(IAllergyService service)
         {
             _service = service;
-            _cache = cache;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
-        {
-            if (!_cache.TryGetValue(AllergiesCacheKey, out var cachedAllergies))
-            {
-                cachedAllergies = await _service.GetAllAsync();
-
-                var cacheOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
-                };
-
-                _cache.Set(AllergiesCacheKey, cachedAllergies, cacheOptions);
-            }
-
-            return Ok(cachedAllergies);
-        }
+            => Ok(await _service.GetAllAsync());
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<IActionResult> Add(string name)
         {
             await _service.AddAsync(name);
-
-            _cache.Remove(AllergiesCacheKey);
-
             return Ok();
         }
 
@@ -53,9 +28,6 @@ namespace Dactra.Controllers
         public async Task<IActionResult> Update(int id, string name)
         {
             await _service.UpdateAsync(id, name);
-
-            _cache.Remove(AllergiesCacheKey);
-
             return Ok();
         }
 
@@ -64,9 +36,6 @@ namespace Dactra.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             await _service.DeleteAsync(id);
-
-            _cache.Remove(AllergiesCacheKey);
-
             return Ok();
         }
     }
