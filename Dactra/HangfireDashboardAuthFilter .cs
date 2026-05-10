6fee5a91da34a045017f017a1430e6d08107b0d6
rashadmo8 <rashadmostafa84@gmail.com>
2026-05-10 20:19:09 +0300

    hangfire ddashbord for production

---
+++ Dactra/HangfireDashboardAuthFilter .cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace Dactra
+{
+    public class HangfireDashboardAuthFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
